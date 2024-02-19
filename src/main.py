5492c0fe0b6ec436d0282537e54396ca53b82ed2
Stan Soldatov <118521851+iwatkot@users.noreply.github.com>
2024-02-19 12:29:41 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import supervisely as sly
 
 from supervisely.app.widgets import Container
 
+import src.globals as g
 import src.ui.input as input
 import src.ui.settings as settings
 import src.ui.output as output
@@ -9,3 +10,6 @@ import src.ui.output as output
 layout = Container(widgets=[input.card, settings.card, output.card])
 
 app = sly.Application(layout=layout)
+
+for _ in range(g.INIT_PROJECTS_COUNT):
+    input.add_project_widgets()
